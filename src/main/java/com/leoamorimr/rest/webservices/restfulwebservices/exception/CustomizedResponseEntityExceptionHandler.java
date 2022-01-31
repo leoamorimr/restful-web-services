4cb4f0059aae2fced0f8756dcd99e9b7b958e694
Leonardo Amorim Ramos <leoamorimr@gmail.com>
2022-01-31 17:20:19 +0000

    Inclusion of Custom Validation using spring-boot-starter-validation API.

--- src/main/java/com/leoamorimr/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/leoamorimr/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -2,8 +2,10 @@ package com.leoamorimr.rest.webservices.restfulwebservices.exception;
 
 import com.leoamorimr.rest.webservices.restfulwebservices.user.UserNotFoundException;
 import org.apache.coyote.Response;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,9 +23,17 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
         ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
     @ExceptionHandler(UserNotFoundException.class)
     public final ResponseEntity<Object> handleUserNotFoundExceptions(Exception ex, WebRequest request) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
+    //Customizing validation errors.
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), "Validation Failed", ex.getBindingResult().toString());
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 }
